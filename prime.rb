d5ab484c2d476e4173008b32ebaae4849376b2a6
Canyon Mathis <canyon.mathis@gmail.com>
2020-02-02 04:11:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+
+array = [2,4,6,8,10]
+
+
+
+
+
+
+def prime?(number)
+  if number < 2
+     return false
+  else 
+    (2..number - 1).to_a.all? do |n|
+      number % n != 0
+    end
+  end 
+end
+
+prime?(-1)
+
+
+
